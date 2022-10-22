cdd2f119d1fbb0bc1346cceca7ff5a8d34d0cf64
Chuck Ocheret <ocheret@panix.com>
2022-10-22 17:56:33 -0400

    Replace big if w/ match. Safe drawing. Basic worm.

--- wormcurses.py
+++ wormcurses.py
@@ -1,5 +1,5 @@
 import curses
-from time import sleep
+import types
 
 import wormstate
 
@@ -14,6 +14,21 @@ The screen will be divided into the following areas:
 +-----------------------------------------------------------------------------+
                     Comments: xxxxxxxxxxxxxxxxxxxxxxxxxxxxx 
 """
+
+# Constants that can be used in a match statement
+keys = types.SimpleNamespace()
+keys.TIMEOUT = -1
+keys.KEY_h = ord('h')
+keys.KEY_H = ord('H')
+keys.KEY_j = ord('j')
+keys.KEY_J = ord('J')
+keys.KEY_k = ord('k')
+keys.KEY_K = ord('K')
+keys.KEY_l = ord('l')
+keys.KEY_L = ord('L')
+keys.KEY_q = ord('q')
+keys.KEY_Q = ord('Q')
+keys.KEY_CTRL_L = ord('L') - 64
 
 class WormCurses(object):
     def set_state(self, state: wormstate.WormState):
@@ -44,27 +59,34 @@ class WormCurses(object):
         # Draw a border around the play area
         border_area = self.stdscr.subwin(self.maxy - 2, self.maxx, 1, 0)
         border_area.box()
+        self.stdscr.noutrefresh()
+
+    def safe_addch(self, y, x, c):
+        try:
+            self.play_area.addch(y, x, c)
+        except curses.error:
+            pass
 
     def draw_worm_full(self):
         w = self.state.worm.left
         c = '@'
         while w != self.state.worm:
             x, y = w.value
-            self.play_area.addch(y, x, c)
+            self.safe_addch(y, x, c)
             c = 'o'
             w = w.left
 
     def draw_worm_update(self, old_xy):
         w = self.state.worm.left
         x, y = w.value
-        self.play_area.addch(y, x, '@')
+        self.safe_addch(y, x, '@')
         w = w.left
         if w != self.state.worm:
             x, y = w.value
-            self.play_area.addch(y, x, 'o')
+            self.safe_addch(y, x, 'o')
         if (old_xy != None):
             x, y = old_xy
-            self.play_area.addch(y, x, ' ')
+            self.safe_addch(y, x, ' ')
 
     def draw_status(self, status):
         try:
@@ -76,10 +98,23 @@ class WormCurses(object):
         except curses.error:
             pass
 
+    def next_step(self):
+        old_xy = self.state.next_step()
+        self.draw_worm_update(old_xy)
+        self.draw_status(f"Iterations {self.counter}.")
+        self.counter += 1
+        self.play_area.refresh()
+        self.stdscr.refresh()
+
+    def draw_all(self):
+        self.draw_static_content()
+        self.stdscr.refresh()
+        self.draw_worm_full()
+        self.play_area.refresh()
+
     def reset_all(self):
         # Get the size of the total screen 
         (self.maxy, self.maxx) = self.stdscr.getmaxyx()
-        self.draw_static_content()
 
         # The first line of the screen will hold the score and growth count
         self.top_line = 0
@@ -91,22 +126,9 @@ class WormCurses(object):
         self.play_maxx = self.maxx - 2  # account for border on both sides
         self.play_maxy = self.maxy - 4  # account for border, header, and footer
         self.state.reset(width = self.maxx - 2, height = self.maxy - 4)
-        # for x in range(self.state.width):
-        #     for y in range(self.state.height):
-        #         try:
-        #             self.play_area.addch(y, x, curses.ACS_BULLET)
-        #         except curses.error:
-        #             pass
-        self.draw_worm_full()
-        self.stdscr.refresh()
 
-    def next_step(self):
-        old_xy = self.state.next_step()
-        self.draw_worm_update(old_xy)
-        self.draw_status(f"Iterations {self.counter}.")
-        self.counter += 1
-        self.play_area.refresh()
-        self.stdscr.refresh()
+        # Draw everything
+        self.draw_all()
 
     def run(self):
         self.counter = 0
@@ -115,21 +137,26 @@ class WormCurses(object):
             self.reset_all()
             while True:
                 ch = self.stdscr.getch()
-                if ch == ord('h') or ch == curses.KEY_LEFT:
-                    self.state.go_left()
-                    self.next_step()
-                elif ch == ord('j') or ch == curses.KEY_DOWN:
-                    self.state.go_down()
-                    self.next_step()
-                elif ch == ord('k') or ch == curses.KEY_UP:
-                    self.state.go_up()
-                    self.next_step()
-                elif ch == ord('l') or ch == curses.KEY_RIGHT:
-                    self.state.go_right()
-                    self.next_step()
-                elif ch == -1:
-                    self.next_step()
-                else:
-                    return
+                match ch:
+                    case keys.TIMEOUT:
+                        self.next_step()
+                    case keys.KEY_h | keys.KEY_H | curses.KEY_LEFT:
+                        self.state.go_left()
+                        self.next_step()
+                    case keys.KEY_j | keys.KEY_J | curses.KEY_DOWN:
+                        self.state.go_down()
+                        self.next_step()
+                    case keys.KEY_k | keys.KEY_K | curses.KEY_UP:
+                        self.state.go_up()
+                        self.next_step()
+                    case keys.KEY_l | keys.KEY_L | curses.KEY_RIGHT:
+                        self.state.go_right()
+                        self.next_step()
+                    case keys.KEY_CTRL_L | curses.KEY_REFRESH:
+                        self.draw_all()
+                    case keys.KEY_q | keys.KEY_Q:
+                        return
+                    case _:
+                        self.draw_status(f"Key is {ch}")
         finally:
             self.teardown_curses()
