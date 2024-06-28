b4acf4c21f3e3aa373dd876f87867a76ed5f1a3d
Chuck Ocheret <ocheret@panix.com>
2024-06-28 16:25:47 -0400

    Fixed documentation to reflect the correct method names.

--- yoke.py
+++ yoke.py
@@ -3,19 +3,18 @@ class Yoke(object):
     used to manipulate the Yokes in the linked list have some
     interesting properties that make this implementation more useful
     than more naive doubly linked list implementations. The algorithms
-    for this implementation originally come from an article from
-    Niklaus Wirth (XXX - can't find the reference).  This class is
-    very powerful but is likely to blow your mind.  It is recommended
-    to work out a few examples by hand to reach an enjoyable epiphany
-    or two.
+    for this implementation was inspired Daniel F. Fisher (dff@acm.org).
+    This class is very powerful but is likely to blow your mind.
+    It is recommended to work out a few examples by hand to reach an enjoyable
+    epiphany or two.
 
     Some of the features include:
 
-    - Reversible insert operations (e.g. a.InsertLeft(b); a.InsertLeft(b)
+    - Reversible insert operations (e.g. a.insert_left(b); a.insert_left(b)
     done together is a no-op).
 
-    - Commutative insert and append operations (i.e. a.InsertLeft(b)
-    and b.InsertLeft(a) produce identical results).
+    - Commutative insert and append operations (i.e. a.insert_left(b)
+    and b.insert_left(a) produce identical results).
 
     - The ability to splice together multiple doubly linked lists
     at any point.
@@ -24,17 +23,17 @@ class Yoke(object):
     two points in the list.
 
     The following examples with string values should illustrate how
-    InsertLeft and InsertRight work.
+    insert_left and insert_right work.
 
     If you have the list segment
 
     ...->a->b->c->... (only showing right pointers)
 
-    and you invoke b.InsertLeft(Yoke("x")) you get...
+    and you invoke b.insert_left(Yoke("x")) you get...
 
     ...->a->x->b->c->...
 
-    However, if you invoke b.InsertRight(Yoke("x")) you get...
+    However, if you invoke b.insert_right(Yoke("x")) you get...
 
     ...->a->b->x->c->...
 
@@ -47,28 +46,28 @@ class Yoke(object):
     p->q->r->s->t->p
     ----------------
 
-    and you do either a.InsertLeft(p) or p.InsertLeft(a), you get
+    and you do either a.insert_left(p) or p.insert_left(a), you get
 
     p->q->r->s->t->a->b->c->d->e->p
     -------------  =============  _
 
-    Invoking a.InsertLeft(p) again, or invoking p.InsertLeft(a)
+    Invoking a.insert_left(p) again, or invoking p.insert_left(a)
     splits the long list back into the original 2 lists again.
 
-    Calling a.InsertRight(p) or p.InsertRight(a) gives you
+    Calling a.insert_right(p) or p.insert_right(a) gives you
 
     a->q->r->s->t->p->b->c->d->e->a
 
-    Invoking a.InsertRight(p) again, or p.InsertRight(a) splits the
+    Invoking a.insert_right(p) again, or p.insert_right(a) splits the
     long lists back into the original 2 lists. Very Cool!
 
     Another way to think of it is that when operating on the two
-    separate lists with a and p, a.InsertLeft(p) splices the entire
+    separate lists with a and p, a.insert_left(p) splices the entire
     list attached to p to the left of a.  What's more, this splicing
     is done in such a way that p is the element from its original
     list that is furthest to the left away from a.
 
-    When there is just one big list, calling a.InsertLeft(p), splits
+    When there is just one big list, calling a.insert_left(p), splits
     the list in two by separating a from its left neighbor and
     separating p from its left neighbor to form two smaller circular
     lists.  Magic.
@@ -93,12 +92,12 @@ class Yoke(object):
         p -> q -> r -> s -> t -> p
         --------------------------
 
-        and you do either a.InsertLeft(p) or p.InsertLeft(a), you get
+        and you do either a.insert_left(p) or p.insert_left(a), you get
 
         p -> q -> r -> s -> t -> a -> b -> c -> d -> e -> p
         ---------------------    =====================    _
 
-        Invoking a.InsertLeft(p) again, or invoking p.InsertLeft(a)
+        Invoking a.insert_left(p) again, or invoking p.insert_left(a)
         splits the long list back into the original 2 lists again.
 
         x is the Yoke to insert in our list.  If x is None then nothing
@@ -126,12 +125,12 @@ class Yoke(object):
         p -> q -> r -> s -> t -> p
         --------------------------
 
-        Calling a.InsertRight(p) or p.InsertRight(a) gives you
+        Calling a.insert_right(p) or p.insert_right(a) gives you
 
         a -> q -> r -> s -> t -> p -> b -> c -> d -> e -> a
         =    ---------------------    =====================
 
-        Invoking a.InsertRight(p) again, or p.InsertRight(a) splits
+        Invoking a.insert_right(p) again, or p.insert_right(a) splits
         the long lists back into the original 2 lists. Very
         Cool!
 
